e665f3ef23103a9894a5c35584db44dcbcd3474e
Daniel Miladinov <daniel.miladinov@gmail.com>
2014-11-28 18:37:49 -0500

    Redefine dropWhile into multiple argument lists to help type inference
    It's a little unfortunate that we need to state that the type of x is
    Int. The first argument to dropWhile is a List[Int], so the function in
    the second argument must accept an Int. Scala can infer this fact if we
    group dropWhile into two argument lists.
    
    The syntax for calling this version of dropWhile looks like
    dropWhile(xs)(f). That is, dropWhile(xs) is returning a function, which
    we then call with the argument f (in other words, dropWhile is
    curried). The main reason for grouping the arguments this way is to
    assist with type inference. We can now use dropWhile without
    annotations:
    
    val xs: List[Int] = List(1,2,3,4,5)
    val ex1 = dropWhile(xs)(x => x < 4)
    
    Note that x is not annotated with its type.
    More generally, when a function definition contains multiple argument
    groups, type information flows from left to right across these argument
    groups.  Here, the first argument group fixes the type parameter A of
    dropWhile to Int, so the annotation on x => x < 4 is not required.
    
    We’ll often group and order our function arguments into multiple
    argument lists to maximize type inference.

--- src/main/scala/fpInScala/exercises/Exercise3Point5.scala
+++ src/main/scala/fpInScala/exercises/Exercise3Point5.scala
@@ -6,8 +6,8 @@ object Exercise3Point5 {
   // Implement dropWhile, which removes elements from the List prefix as long as they match a predicate.
 
   @annotation.tailrec
-  def dropWhile [A] (l: List[A], f: A => Boolean): List[A] = l match {
-    case Nil => Nil
-    case Cons(h, t) => if (f(h)) dropWhile(t, f) else l
+  def dropWhile [A] (l: List[A])(f: A => Boolean): List[A] = l match {
+    case Cons(h, t) if f(h) => dropWhile(t)(f)
+    case _ => l
   }
 }
--- src/test/scala/fpInScala/exercises/Exercise3Point5Test.scala
+++ src/test/scala/fpInScala/exercises/Exercise3Point5Test.scala
@@ -10,18 +10,18 @@ class Exercise3Point5Test extends FlatSpec with ShouldMatchers {
   private val never: (Any) => Boolean = (n: Any) => false
 
   "An Exercise 3.5 Solution" should "return Nil whenever passed Nil, regardless of the predicate" in {
-    dropWhile(Nil, always) should be (Nil)
+    dropWhile(Nil)(always) should be (Nil)
   }
 
   it should "return Nil when given an always-truthy predicate" in {
-    dropWhile(List(1, 2, 3), always) should be (Nil)
+    dropWhile(List(1, 2, 3))(always) should be (Nil)
   }
 
   it should "return the original list when given a never-truthy predicate" in {
-    dropWhile(List(1, 2, 3), never) should be (List(1, 2, 3))
+    dropWhile(List(1, 2, 3))(never) should be (List(1, 2, 3))
   }
 
   it should "stop dropping at the first list element that fails the predicate" in {
-    dropWhile(List(1, 2, 3, 4, 5, 4, 3, 2, 1), (n: Int) => n < 5) should be (List(5, 4, 3, 2, 1))
+    dropWhile(List(1, 2, 3, 4, 5, 4, 3, 2, 1))(n => n < 5) should be (List(5, 4, 3, 2, 1))
   }
 }
