9e843cddc1d3cc62b98049190ed253018f513438
Daniel Miladinov <daniel.miladinov@gmail.com>
2014-11-29 17:09:05 -0500

    Add tests to cover the behavior of List.sum and List.product

---
+++ src/test/scala/fpInScala/dataStructures/CompanionObjectTests.scala
@@ -0,0 +1,54 @@
+package fpInScala.dataStructures
+
+import org.scalatest.prop.TableDrivenPropertyChecks
+import org.scalatest.{FlatSpec, ShouldMatchers}
+
+class CompanionObjectTests extends FlatSpec with ShouldMatchers with TableDrivenPropertyChecks {
+
+  val expectedSums = Table(
+    ("inputs",                                        "expected sums") ,
+    (Array(0),                                         0),
+    (Array(0, 1),                                      1),
+    (Array(0, 1, 2),                                   3),
+    (Array(0, 1, 2, 3),                                6),
+    (Array(0, 1, 2, 3, 4),                            10),
+    (Array(0, 1, 2, 3, 4, 5),                         15),
+    (Array(0, 1, 2, 3, 4, 5, 6),                      21),
+    (Array(0, 1, 2, 3, 4, 5, 6, 7),                   28),
+    (Array(0, 1, 2, 3, 4, 5, 6, 7, 8),                36),
+    (Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9),             45),
+    (Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10),         55),
+    (Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11),     66),
+    (Array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12), 78)
+  )
+
+  forAll (expectedSums) { (inputs: Array[Int], expectedSum: Int) =>
+    val list = List(inputs:_*)
+    List.sum(list) should be === expectedSum
+  }
+
+
+  val expectedProducts = Table(
+    ("inputs",                                                                   "expected products"),
+    (Array(0.0),                                                                         0.0),
+    (Array(1.0),                                                                         1.0),
+    (Array(1.0, 2.0),                                                                    2.0),
+    (Array(1.0, 2.0, 3.0),                                                               6.0),
+    (Array(1.0, 2.0, 3.0, 4.0),                                                         24.0),
+    (Array(1.0, 2.0, 3.0, 4.0, 5.0),                                                   120.0),
+    (Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0),                                              720.0),
+    (Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0),                                        5040.0),
+    (Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0),                                  40320.0),
+    (Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0),                            362880.0),
+    (Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0),                     3628800.0),
+    (Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0),              39916800.0),
+    (Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0),       479001600.0),
+    (Array(0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0,  9.0, 10.0, 11.0, 12.0),         0.0),
+    (Array(1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0,  0.0),         0.0)
+  )
+
+  forAll (expectedProducts) { (inputs: Array[Double], expectedProduct: Double) =>
+    val list = List(inputs:_*)
+    List.product(list) should be === expectedProduct
+  }
+}
