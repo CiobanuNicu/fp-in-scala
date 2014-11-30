c9dd5eb0790c52a0fb39191b2d179cd0f3bb560e
Daniel Miladinov <daniel.miladinov@gmail.com>
2014-11-30 18:12:01 -0500

    Solve Exercise 3.7

---
+++ src/main/scala/fpInScala/exercises/Exercise3Point7.scala
@@ -0,0 +1,10 @@
+package fpInScala.exercises
+
+object Exercise3Point7 {
+
+  // Can product, implemented using foldRight, immediately halt the recursion and return 0.0 if it encounters a 0.0?
+  // Why or why not? Consider how any short-circuiting might work if you call foldRight with a large list.
+  // This is a deeper question that we’ll return to in chapter 5.
+
+  // No, it's not possible given the current implementation of foldRight.
+}
