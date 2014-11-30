55d86f444348fbab627f0c76ba8f39225ffdeb16
Daniel Miladinov <daniel.miladinov@gmail.com>
2014-11-30 18:02:20 -0500

    Add List.foldRight
    We use this function to generalize applying some function and an
    initial value to iteratively reduce a list to a single value.
    
    We can also redefine our sum and product functions, in which there is
    some duplication among them, in terms of foldRight, to reduce that
    duplication and make our code more concise.

--- src/main/scala/fpInScala/dataStructures/List.scala
+++ src/main/scala/fpInScala/dataStructures/List.scala
@@ -5,6 +5,11 @@ case object Nil extends List[Nothing]
 case class Cons [+A] (head: A, tail: List[A]) extends List[A]
 
 object List {
+  def foldRight [A, B] (as: List[A], z: B) (f: (A, B) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
+
   def sum (ints: List[Int]): Int = ints match {
     case Nil => 0
     case Cons(x, xs) => x + sum(xs)
