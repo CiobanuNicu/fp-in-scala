fa4ad9a14861bd67f921284f67a75a005b53ac3b
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-04-29 19:04:14 -0400

    Solve Exercise 6.4

---
+++ src/main/scala/fpInScala/exercises/chapter6/Exercise4.scala
@@ -0,0 +1,19 @@
+package fpInScala.exercises.chapter6
+
+import fpInScala.purelyFunctionalState.RNG
+
+object Exercise4 {
+  // Write a function to generate a list of random integers.
+  def ints (count: Int) (rng: RNG): (List[Int], RNG) = {
+    @annotation.tailrec
+    def go (c: Int) (s: (List[Int], RNG)): (List[Int], RNG) = {
+      if (c <= 0) s
+      else {
+        val (l, r) = s
+        val (i, r1) = Exercise1.nonNegativeInt(r)
+        go(c - 1)(i :: l, r1)
+      }
+    }
+    go(count)(List(), rng)
+  }
+}
---
+++ src/test/scala/fpInScala/exercises/chapter6/Exercise4Test.scala
@@ -0,0 +1,16 @@
+package fpInScala.exercises.chapter6
+
+import fpInScala.purelyFunctionalState.SimpleRNG
+import org.scalacheck.Properties
+import org.scalacheck.Prop.forAll
+
+class Exercise4Test extends Properties("Exercise4.ints") {
+  property("Produces a list of random integers, of the count specified") = forAll { (n: Int, c: Int) =>
+    val rng = SimpleRNG(n)
+    val count = Math.abs(c - 1) % 500 // Keep the lists short-ish
+
+    val (randomInts, _) = Exercise4.ints(count)(rng)
+
+    randomInts.size == count && randomInts.forall(i => i >= 0 && i <= Int.MaxValue)
+  }
+}
