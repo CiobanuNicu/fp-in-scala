b4681f6f7e07b9b296bbb61260361f04ce58e88b
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-08-16 21:21:11 -0400

    Solve Exercise 7.1
    Par.map2 is a new higher-order function for combining the result of two
    parallel computations. What is its signature? Give the most general
    signature possible (don’t assume it works only for Int).

--- src/main/scala/fpInScala/dataStructures/parallel/Par.scala
+++ src/main/scala/fpInScala/dataStructures/parallel/Par.scala
@@ -4,6 +4,8 @@ object Par {
   def unit [A] (a: => A): Par[A] = ???
 
   def get [A] (a: Par[A]): A = ???
+
+  def map2 [A, B, C] (parL: Par[A], parR: Par[B]) (f: (A, B) => C): Par[C] = ???
 }
 
 trait Par [A] { }
