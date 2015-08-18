3ee23130a0671f48368f28791e3c7a50ab454f92
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-08-17 21:09:26 -0400

    Add some more combinators for working with Par

--- src/main/scala/fpInScala/dataStructures/parallel/Par.scala
+++ src/main/scala/fpInScala/dataStructures/parallel/Par.scala
@@ -1,11 +1,23 @@
 package fpInScala.dataStructures.parallel
 
 object Par {
-  def unit [A] (a: => A): Par[A] = ???
+  // Creates a computation that immediately results in the value a,
+  // promoting a constant value to a parallel computation.
+  def unit [A] (a: A): Par[A] = ???
 
-  def get [A] (a: Par[A]): A = ???
-
+  // Combines the result of two parallel computations with a binary function
   def map2 [A, B, C] (parL: Par[A], parR: Par[B]) (f: (A, B) => C): Par[C] = ???
+
+  // Marks a computation for concurrent evaluation by run.
+  // The evaluation won’t actually occur until forced by run.
+  def fork [A] (a: => Par[A]): Par[A] = ???
+
+  // Wraps its unevaluated argument in a Par and marks it for concurrent evaluation by run
+  def lazyUnit [A] (a: => A): Par[A] = fork(unit(a))
+
+  // Fully evaluates a given Par, spawning parallel computations
+  // as requested by fork and extracting the resulting value.
+  def run [A] (a: Par[A]): A = ???
 }
 
 trait Par [A] { }
