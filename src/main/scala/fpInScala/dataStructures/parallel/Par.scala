fdaab5fece1493e6a42a510b035321fb32457377
Daniel Miladinov <daniel.miladinov@gmail.com>
2015-08-19 21:02:42 -0400

    Solve Exercise 7.3
    . . . with some help from the authors!

--- src/main/scala/fpInScala/dataStructures/parallel/Par.scala
+++ src/main/scala/fpInScala/dataStructures/parallel/Par.scala
@@ -32,10 +32,37 @@ object Par {
   // if we want the evaluation of f to occur in a separate thread
   def map2 [A, B, C] (a: Par[A], b: Par[B]) (f: (A, B) => C): Par[C] =
     (es: ExecutorService) => {
-      val af = a(es)
-      val bf = b(es)
-      UnitFuture(f(af.get, bf.get))
+      val (af, bf) = (a(es), b(es))
+      Map2Future(af, bf, f)
     }
+
+  case class Map2Future [A, B, C] (a: Future[A], b: Future[B], f: (A, B) => C) extends Future[C] {
+    @volatile var cache: Option[C] = None
+
+    def isDone: Boolean = cache.isDefined
+    def isCancelled: Boolean = a.isCancelled || b.isCancelled
+
+    def cancel (mayInterruptIfRunning: Boolean): Boolean = a.cancel(mayInterruptIfRunning) || b.cancel(mayInterruptIfRunning)
+
+    def get (): C = compute(Long.MaxValue)
+    def get (timeout: Long, unit: TimeUnit): C = compute(TimeUnit.NANOSECONDS.convert(timeout, unit))
+
+    private def compute (timeoutInNanoseconds: Long): C = cache match {
+      case Some(c) => c
+      case None =>
+        val start = System.nanoTime
+        val ar = a.get(timeoutInNanoseconds, TimeUnit.NANOSECONDS)
+        val stop = System.nanoTime
+
+        val aTime = stop - start
+        val br = b.get(timeoutInNanoseconds - aTime, TimeUnit.NANOSECONDS)
+        val ret = f(ar, br)
+
+        cache = Some(ret)
+        ret
+    }
+
+  }
 
   // Marks a computation for concurrent evaluation by run.
   // The evaluation won’t actually occur until forced by run.
