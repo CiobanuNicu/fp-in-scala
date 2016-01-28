ba59027f1702241bfe8fea2fe7f9fb5f90838ae1
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-01-27 21:49:23 -0500

    Return to Prop and begin implementing it

--- src/main/scala/fpInScala/testing/Prop.scala
+++ src/main/scala/fpInScala/testing/Prop.scala
@@ -1,8 +1,30 @@
 package fpInScala.testing
 
-trait Prop {
-  def check: Boolean
-  def && (p: Prop): Prop = new Prop {
-    def check = Prop.this.check && p.check
+import fpInScala.testing.Prop._
+import fpInScala.purelyFunctionalState.RNG
+import fpInScala.dataStructures.stream.Stream
+
+case class Prop (run: (TestCases, RNG) => Result)
+
+object Prop {
+  type FailedCase = String
+  type SuccessCount = Int
+  type TestCases = Int
+  type Result = Option[(FailedCase, SuccessCount)]
+
+  def forAll [A] (as: Gen[A]) (f: A => Boolean): Prop = Prop {
+    (n: TestCases, rng: RNG) => randomStream(as)(rng).zip(Stream.from(0)).take(n).map {
+      case (a, i) => try {
+        if (f(a)) Passed else Falsified(a.toString, i)
+      } catch { case e: Exception => Falsified(buildMsg(a, e), i) }
+    }.find(_.isFalsified).getOrElse(Passed)
   }
+
+  def randomStream [A] (g: Gen[A]) (rng: RNG): Stream[A] =
+    Stream.unfold(rng)(rng => Some(g.sample.run(rng)))
+
+  def buildMsg [A] (s: A, e: Exception): String =
+    s"test case $s\n" +
+    s"generated an exception: ${e.getMessage}\n" +
+    s"stack trace:\n ${e.getStackTrace.mkString("\n")}"
 }
