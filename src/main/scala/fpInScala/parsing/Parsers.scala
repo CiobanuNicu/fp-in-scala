063f8b54c179d324573085eaaae027d321eeb538
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-02-08 19:12:54 -0500

    Add two new combinators, many and map

--- src/main/scala/fpInScala/parsing/Parsers.scala
+++ src/main/scala/fpInScala/parsing/Parsers.scala
@@ -7,6 +7,9 @@ trait Parsers [ParseError, Parser[+_]] { self =>
   def run [A] (p: Parser[A]) (input: String): Either[ParseError, A]
   def char (c: Char): Parser[Char]
 
+  def many [A] (p: Parser[A]): Parser[List[A]]
+  def map [A, B] (p: Parser[A]) (f: A => B): Parser[B]
+
   def or [A] (s1: Parser[A], s2: Parser[A]): Parser[A]
   implicit def string (s: String): Parser[String]
   implicit def operators [A] (p: Parser[A]): ParserOps[A] = ParserOps[A](p)
@@ -15,5 +18,7 @@ trait Parsers [ParseError, Parser[+_]] { self =>
   case class ParserOps [A] (p: Parser[A]) {
     def | [B >: A] (p2: Parser[B]): Parser[B] = self.or(p, p2)
     def or [B >: A] (p2: Parser[B]): Parser[B] = self.or(p, p2)
+    def many: Parser[List[A]] = self.many(p)
+    def map [B] (f: A => B): Parser[B] = self.map(p)(f)
   }
 }
