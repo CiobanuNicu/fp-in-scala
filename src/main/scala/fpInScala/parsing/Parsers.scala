5d55c778ee54ed946f62853da37562463bc0ef54
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-02-10 08:23:37 -0500

    Now that we have map, we can implement char in terms of string and map

--- src/main/scala/fpInScala/parsing/Parsers.scala
+++ src/main/scala/fpInScala/parsing/Parsers.scala
@@ -7,7 +7,7 @@ import fpInScala.testing.Prop._
 trait Parsers [ParseError, Parser[+_]] { self =>
 
   def run [A] (p: Parser[A]) (input: String): Either[ParseError, A]
-  def char (c: Char): Parser[Char]
+  def char (c: Char): Parser[Char] = string(c.toString) map (_.charAt(0))
 
   def many [A] (p: Parser[A]): Parser[List[A]]
   def map [A, B] (p: Parser[A]) (f: A => B): Parser[B]
