ff550fbb9a3040a9310e21b7b90c72f852eb871f
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-02-23 08:58:48 -0500

    Add an implicit def for converting a regex into a parser

--- src/main/scala/fpInScala/parsing/Parsers.scala
+++ src/main/scala/fpInScala/parsing/Parsers.scala
@@ -4,6 +4,8 @@ import scala.language.{implicitConversions, higherKinds}
 import fpInScala.testing._
 import fpInScala.testing.Prop._
 
+import scala.util.matching.Regex
+
 trait Parsers [ParseError, Parser[+_]] { self =>
 
   def run [A] (p: Parser[A]) (input: String): Either[ParseError, A]
@@ -28,6 +30,7 @@ trait Parsers [ParseError, Parser[+_]] { self =>
   implicit def string (s: String): Parser[String]
   implicit def operators [A] (p: Parser[A]): ParserOps[A] = ParserOps[A](p)
   implicit def asStringParser [A] (a: A) (implicit f: A => Parser[String]): ParserOps[String] = ParserOps(f(a))
+  implicit def regex (r: Regex): Parser[String]
 
   case class ParserOps [A] (p: Parser[A]) {
     def | [B >: A] (p2: Parser[B]): Parser[B] = self.or(p, p2)
