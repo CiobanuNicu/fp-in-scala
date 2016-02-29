468041fe1173e8f95255ff5f7cd32f223a29bdd9
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-02-29 17:51:54 -0500

    Add a method stub for where the Solution for Exercise 9.9 should go

--- src/main/scala/fpInScala/parsing/JSON.scala
+++ src/main/scala/fpInScala/parsing/JSON.scala
@@ -1,5 +1,7 @@
 package fpInScala.parsing
 
+import scala.language.higherKinds
+
 trait JSON
 
 object JSON {
@@ -9,4 +11,7 @@ object JSON {
   case class JBool (get: Boolean) extends JSON
   case class JArray (get: IndexedSeq[JSON]) extends JSON
   case class JObject (get: Map[String, JSON]) extends JSON
+
+  // Stub out the method that would contain the solution . . . I guess we can use Throwable for the ParseError type?
+  def jsonParser [Parser[+_]] (P: Parsers[Throwable, Parser]): Parser[JSON] = ???
 }
