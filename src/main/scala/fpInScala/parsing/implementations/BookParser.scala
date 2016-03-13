15a1496d7a99dcb1f8c8abd720760d4557e6c7e3
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-03-12 21:44:10 -0500

    Add BookParser.Result.mapError

--- src/main/scala/fpInScala/parsing/implementations/BookParser.scala
+++ src/main/scala/fpInScala/parsing/implementations/BookParser.scala
@@ -9,7 +9,13 @@ class BookParser [+A]
 object BookParser extends Parsers[BookParser] {
   type Parser[+A] = Location => Result[A]
 
-  trait Result[+A]
+  trait Result[+A] {
+    def mapError (f: ParseError => ParseError): Result[A] = this match {
+      case Failure(e) => Failure(f(e))
+      case _ => this
+    }
+  }
+
   case class Success[+A] (get: A, charsConsumed: Int) extends Result[A]
   case class Failure (get: ParseError) extends Result[Nothing]
 
