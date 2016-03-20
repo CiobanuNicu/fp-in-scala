575558c79a2fbff504bbf9b895b9cb28db5ccb74
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-03-20 19:23:48 -0400

    Add Monoid instances for String and List

--- src/main/scala/fpInScala/monoids/Monoid.scala
+++ src/main/scala/fpInScala/monoids/Monoid.scala
@@ -4,3 +4,15 @@ trait Monoid [A] {
   def op (a1: A, a2: A): A
   def zero: A
 }
+
+object Monoid {
+  val stringMonoid = new Monoid[String] {
+    def op (a1: String, a2: String): String = a1 + a2
+    val zero: String = ""
+  }
+
+  def listMonoid [A] = new Monoid[List[A]] {
+    def op (a1: List[A], a2: List[A]): List[A] = a1 ++ a2
+    val zero: List[A] = Nil
+  }
+}
