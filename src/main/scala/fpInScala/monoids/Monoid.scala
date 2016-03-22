ce3cab8f11f35f6b9ca83bb9379661447c90810f
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-03-22 16:38:40 -0400

    Solve Exercise 10.1

--- src/main/scala/fpInScala/monoids/Monoid.scala
+++ src/main/scala/fpInScala/monoids/Monoid.scala
@@ -11,6 +11,26 @@ object Monoid {
     val zero: String = ""
   }
 
+  val intAddition: Monoid[Int] = new Monoid[Int] {
+    def op (a1: Int, a2: Int): Int = a1 + a2
+    val zero: Int = 0
+  }
+
+  val intMultiplication: Monoid[Int] = new Monoid[Int] {
+    def op (a1: Int, a2: Int): Int = a1 * a2
+    val zero: Int = 1
+  }
+
+  val booleanOr: Monoid[Boolean] = new Monoid[Boolean] {
+    def op (a1: Boolean, a2: Boolean): Boolean = a1 || a2
+    val zero: Boolean = false
+  }
+
+  val booleanAnd: Monoid[Boolean] = new Monoid[Boolean] {
+    def op (a1: Boolean, a2: Boolean): Boolean = a1 && a2
+    val zero: Boolean = true
+  }
+
   def listMonoid [A] = new Monoid[List[A]] {
     def op (a1: List[A], a2: List[A]): List[A] = a1 ++ a2
     val zero: List[A] = Nil
