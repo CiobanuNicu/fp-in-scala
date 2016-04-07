39a54a9a316a24bc97b05244d22e56e000c30bcc
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-04-07 08:20:12 -0400

    Solve Exericse 10.12
    . . . with some help

--- src/main/scala/fpInScala/monoids/Foldable.scala
+++ src/main/scala/fpInScala/monoids/Foldable.scala
@@ -3,11 +3,49 @@ package fpInScala.monoids
 import scala.language.higherKinds
 
 trait Foldable[F[_]] {
-  def foldRight [A, B] (as: F[A]) (z: B) (f: (A, B) => B): B
+  import Monoid._
 
-  def foldLeft [A, B] (as: F[A]) (z: B) (f: (B, A) => B): B
+  def foldRight [A, B] (as: F[A]) (z: B) (f: (A, B) => B): B =
+    foldMap(as)(f.curried)(endoMonoid[B])(z)
 
-  def foldMap [A, B] (as: F[A]) (f: A => B) (mb: Monoid[B]): B
+  def foldLeft [A, B] (as: F[A]) (z: B) (f: (B, A) => B): B =
+    foldMap(as)(a => (b: B) => f(b, a))(dual(endoMonoid[B]))(z)
 
-  def concatenate [A] (as: F[A]) (m: Monoid[A]): A = foldLeft(as)(m.zero)(m.op)
+  def foldMap [A, B] (as: F[A]) (f: A => B) (mb: Monoid[B]): B =
+    foldRight(as)(mb.zero)((a, b) => mb.op(f(a), b))
+
+  def concatenate [A] (as: F[A]) (m: Monoid[A]): A =
+    foldLeft(as)(m.zero)(m.op)
+}
+
+object ListFoldable extends Foldable[List] {
+  override def foldRight [A, B] (as: List[A]) (z: B) (f: (A, B) => B): B =
+    as.foldRight(z)(f)
+
+  override def foldLeft [A, B] (as: List[A]) (z: B) (f: (B, A) => B): B =
+    as.foldLeft(z)(f)
+
+  override def foldMap [A, B] (as: List[A]) (f: (A) => B) (mb: Monoid[B]): B =
+    as.foldLeft(mb.zero)((b, a) => mb.op(b, f(a)))
+}
+
+object IndexedSeqFoldable extends Foldable[IndexedSeq] {
+  import Monoid._
+
+  override def foldRight [A, B] (as: IndexedSeq[A]) (z: B) (f: (A, B) => B): B =
+    as.foldRight(z)(f)
+
+  override def foldLeft [A, B] (as: IndexedSeq[A]) (z: B) (f: (B, A) => B): B =
+    as.foldLeft(z)(f)
+
+  override def foldMap [A, B] (as: IndexedSeq[A]) (f: (A) => B) (mb: Monoid[B]): B =
+    foldMapV(as, mb)(f)
+}
+
+object StreamFoldable extends Foldable[Stream] {
+  override def foldRight [A, B] (as: Stream[A]) (z: B) (f: (A, B) => B): B =
+    as.foldRight(z)(f)
+
+  override def foldLeft [A, B] (as: Stream[A]) (z: B) (f: (B, A) => B): B =
+    as.foldLeft(z)(f)
 }
