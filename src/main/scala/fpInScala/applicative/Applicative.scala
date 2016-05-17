d66a523d60be5300ed674ea774d57590039992be
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-05-16 21:26:47 -0400

    Start a new chapter on Applicatives and Traversable Applicatives
     . . . and add the Applicative trait.

---
+++ src/main/scala/fpInScala/applicative/Applicative.scala
@@ -0,0 +1,15 @@
+package fpInScala.applicative
+
+import fpInScala.monads.Functor
+import scala.language.higherKinds
+
+trait Applicative [F[_]] extends Functor[F] {
+  // primitive combinators
+  def map2 [A, B, C] (fa: F[A], fb: F[B]) (f: (A, B) => C): F[C]
+  def unit [A] (a: => A): F[A]
+
+  // derived combinators
+  def map [A, B] (fa: F[A]) (f: A => B): F[B] = map2(fa, unit(()))((a, _) => f(a))
+  def traverse [A, B] (as: List[A]) (f: A => F[B]): F[List[B]] =
+    as.foldRight(unit(List[B]()))((a, fbs) => map2(f(a), fbs)(_ :: _))
+}
