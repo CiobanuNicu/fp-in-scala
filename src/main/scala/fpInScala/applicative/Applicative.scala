e25cb1649370f2087c05748114938e71eeb69e80
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-05-17 08:27:43 -0400

    Solve Exercise 12.1, Part I

--- src/main/scala/fpInScala/applicative/Applicative.scala
+++ src/main/scala/fpInScala/applicative/Applicative.scala
@@ -12,4 +12,6 @@ trait Applicative [F[_]] extends Functor[F] {
   def map [A, B] (fa: F[A]) (f: A => B): F[B] = map2(fa, unit(()))((a, _) => f(a))
   def traverse [A, B] (as: List[A]) (f: A => F[B]): F[List[B]] =
     as.foldRight(unit(List[B]()))((a, fbs) => map2(f(a), fbs)(_ :: _))
+
+  def sequence [A] (fas: List[F[A]]): F[List[A]] = traverse(fas)(a => a)
 }
