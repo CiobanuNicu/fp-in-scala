73fa8a0c59ceda2f6f6a627a23fd044eaf4bfc3d
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-05-20 08:31:45 -0400

    Solve Exercise 12.2, Part I
    . . . with some help

--- src/main/scala/fpInScala/applicative/Applicative.scala
+++ src/main/scala/fpInScala/applicative/Applicative.scala
@@ -17,3 +17,12 @@ trait Applicative [F[_]] extends Functor[F] {
   def replicateM [A] (n: Int, fa: F[A]): F[List[A]] = sequence(List.fill(n)(fa))
   def product [A, B] (fa: F[A], fb: F[B]): F[(A, B)] = map2(fa, fb)((_, _))
 }
+
+trait ApplicativeWithUnitAndApplyAsPrimitives [F[_]] extends Functor[F] {
+  def apply [A, B] (fab: F[A => B]) (fa: F[A]): F[B] = map2(fab, fa) (_(_))
+
+  def unit [A] (a: => A): F[A]
+
+  def map [A, B] (fa: F[A]) (f: A => B): F[B]
+  def map2 [A, B, C] (fa: F[A], fb: F[B]) (f: (A, B) => C): F[C]
+}
