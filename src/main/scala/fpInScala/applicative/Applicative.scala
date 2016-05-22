cefcf41c771d6dc2de72ccdf8c26cafafa886094
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-05-22 12:34:06 -0400

    Solve Exercise 12.2, Part III
    . . . with some help
    No fair, the answer key had an error in it too, so it wasn't all that
    much help after all . . .

--- src/main/scala/fpInScala/applicative/Applicative.scala
+++ src/main/scala/fpInScala/applicative/Applicative.scala
@@ -24,5 +24,5 @@ trait ApplicativeWithUnitAndApplyAsPrimitives [F[_]] extends Functor[F] {
   def unit [A] (a: => A): F[A]
 
   def map [A, B] (fa: F[A]) (f: A => B): F[B] = apply(unit(f))(fa)
-  def map2 [A, B, C] (fa: F[A], fb: F[B]) (f: (A, B) => C): F[C]
+  def map2 [A, B, C] (fa: F[A], fb: F[B]) (f: (A, B) => C): F[C] = apply(map(fa)(f.curried))(fb)
 }
