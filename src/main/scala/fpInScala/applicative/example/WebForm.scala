3841dfbbc12d3070c4ee35ad42edad1013bf257f
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-06-02 12:28:12 -0400

    Add an example of Applicative Validation in action

---
+++ src/main/scala/fpInScala/applicative/example/WebForm.scala
@@ -0,0 +1,34 @@
+package fpInScala.applicative.example
+
+import java.util.Date
+
+import fpInScala.applicative.{Applicative, Failure, Success, Validation}
+
+case class WebForm(name: String, birthdate: Date, phoneNumber: String)
+
+object WebForm {
+  def validName (name: String): Validation[String, String] =
+    if (name != "") Success(name)
+    else Failure("Name cannot be empty")
+
+  def validBirthdate (birthdate: String): Validation[String, Date] =
+    try {
+      import java.text._
+      Success(new SimpleDateFormat("yyyy-MM-dd").parse(birthdate))
+    } catch {
+      case _: Throwable => Failure("Birthdate must be in the form yyyy-MM-dd")
+    }
+
+  def validPhone (phoneNumber: String): Validation[String, String] =
+    if (phoneNumber.matches("[0-9]{10}")) Success(phoneNumber)
+    else Failure("Phone number must be 10 digits")
+
+  def validWebForm(name: String, birthdate: String, phone: String): Validation[String, WebForm] = {
+    val validationApplicative = Applicative.validationApplicative[String]
+    validationApplicative.map3(
+      validName(name),
+      validBirthdate(birthdate),
+      validPhone(phone)
+    )(WebForm(_, _, _))
+  }
+}
